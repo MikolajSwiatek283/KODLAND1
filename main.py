5e9ca4fb3293ece419c4df5b7039fa3503cdee96
MikolajSwiatek283 <159059238+MikolajSwiatek283@users.noreply.github.com>
2024-02-26 19:08:24 +0100

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-meme_dict = {
-            "CRINGE": "coś dziwnego lub wstydliwego",
-            "LOL": "odpowiedź na coś zabawnego",
-            "SHEESH": "lekka dezaprobata",
-            "CREEPY": "straszny, złowieszczy",
-            "AGGRO": "stać się agresywnym/zły"
-            }
-while True:
-    word = input('Wpisz słowo, którego nie rozumiesz')
-    if word in meme_dict.keys():
-       print(meme_dict[word])
-    else:
-        print("Nie znaleziono tego słowa")
+from random import randint
+import discord
+from bot_logic import *
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def Cześć(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def hehe(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def rzut_monetą(ctx):
+    await ctx.send(flip_coin())
+
+@bot.command()
+async def emotka(ctx):
+    await ctx.send(gen_emodji())
+
+@bot.command()
+async def liczba(ctx):
+    await ctx.send(random.randint(1, 10))
+
+bot.run('MTIwOTE4OTA1NDk0NDY0OTM1OQ.GGxlH8.p6_yRBziiY0rF1zQ3DSLHTTyH4YEFSXiqBrYuU')
